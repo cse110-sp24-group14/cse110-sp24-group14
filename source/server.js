e7bc395bccc0a724317d08431e3271d9f595c61c
Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
2024-05-22 11:21:30 -0700

    Serve static html (#53)

--- source/server.js
+++ source/server.js
@@ -59,6 +59,8 @@ const server = http.createServer((req, res) => {
     // Update the condition for serving JavaScript files
     } else if (req.url.endsWith('.js') && req.method === 'GET') {
         serveStaticFile(res, req.url.slice(1), 'text/javascript');
+    } else if (req.url.endsWith('.html') && req.method === 'GET') {
+        serveStaticFile(res, req.url.slice(1), 'text/html');
 
     } else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
