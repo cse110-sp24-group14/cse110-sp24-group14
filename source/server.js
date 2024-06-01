adc1b25ecc00e00f5c0f179efd6075de9405b163
OasisAlex <114462126+OasisAlex@users.noreply.github.com>
2024-06-01 09:04:56 -0700

    populate-tasks-calendar

--- source/server.js
+++ source/server.js
@@ -3,6 +3,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import http from 'http'
 import fs from 'fs'
+import url from 'url'
 // Derive __dirname
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -26,10 +27,29 @@ const fetchTasks = (callback) => {
         }
     });
 };
-
+/**
+ * Gets tasks for the current month you are at
+ * 
+ * @param {year, month, Function} callback 
+ */
+const fetchTasksDue = (year, month, callback) => {
+    const sqlQuery = 'SELECT * FROM Tasks WHERE YEAR(due_date) = ? AND MONTH(due_date) = ?';
+    console.log('Executing query:', sqlQuery, 'with parameters:', year, month);
+    connection.execute(sqlQuery, [year, month], (error, results) => {
+        if (error) {
+            console.error('Error fetching tasks for month:', error);
+            callback(error, null);
+        } else {
+            callback(null, results);
+        }
+    });
+};
 
 // Create an HTTP server
 const server = http.createServer((req, res) => {
+    const parsedUrl = url.parse(req.url, true); // Parse the URL
+    const pathname = parsedUrl.pathname;
+    const query = parsedUrl.query;
     if (req.url === '/tasks' && req.method === 'GET') {
         fetchTasks((err, users) => {
             if (err) {
@@ -40,7 +60,25 @@ const server = http.createServer((req, res) => {
                 res.end(JSON.stringify(users));
             }
         });
-
+    } else if (pathname === '/tasks-this-month' && req.method === 'GET') {
+        const year = parseInt(query.year, 10);
+        const month = parseInt(query.month, 10);
+        console.log('Received request for tasks this month:', year, month);
+        // Ensure year and month are valid
+        if (!isNaN(year) && !isNaN(month)) {
+            fetchTasksDue(year, month, (err, tasks) => {
+                if (err) {
+                    res.writeHead(500, { 'Content-Type': 'application/json' });
+                    res.end(JSON.stringify({ error: 'Internal Server Error' }));
+                } else {
+                    res.writeHead(200, { 'Content-Type': 'application/json' });
+                    res.end(JSON.stringify(tasks));
+                }
+            });
+        } else {
+            res.writeHead(400, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify({ error: 'Bad Request' }));
+        }
     } else if (req.url === '/' && req.method === 'GET') {
         fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
             if (err) {
