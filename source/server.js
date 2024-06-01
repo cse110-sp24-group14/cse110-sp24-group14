8771b7c0ad89395db76c005155f0791a168026e8
EdwardLinS <63514282+EdwardLinS@users.noreply.github.com>
2024-05-31 21:11:22 -0700

    Add pathname for updating task completion

--- source/server.js
+++ source/server.js
@@ -101,7 +101,17 @@ export const server = http.createServer((req, res) => {
                 res.end(JSON.stringify(numCompleted));
             }
         });
-    
+    } else if (pathname === '/updated-task-completion' && req.method === 'POST') {
+        updateTaskCompletion(taskId, completed, (err, result) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ error: 'Internal Server Error' }));
+            } else {
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(numCompleted));
+            }
+        })
+
 
     } else if (req.url === '/' && req.method === 'GET') {
         fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
