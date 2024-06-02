aacc9cc416f32a146825e63f21323b8afec418f9
Advaith Modali <advaith0331@gmail.com>
2024-06-02 00:37:29 -0700

    fix linting errors

--- source/snippetCreation/snippetCreation.js
+++ source/snippetCreation/snippetCreation.js
@@ -1,37 +1,33 @@
 // event listener to handle DOM Content load first upon which requests can be made
 document.addEventListener('DOMContentLoaded', () => {
-  const snippetForm = document.getElementById('snippetForm');
-  snippetForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const code = document.getElementById('codeInput').value;
-    const language = document.getElementById('languageSelect').value;
-    console.log(code);
-    console.log(language);
+    const snippetForm = document.getElementById('snippetForm');
+    snippetForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const code = document.getElementById('codeInput').value;
+        const language = document.getElementById('languageSelect').value;
 
-    snippetCompleted(code, language);
-    displaySnippets();
-  })
+        snippetCompleted(code, language);
+    })
 });
 
 //create a POST request using fetch on the frontend
 const snippetCompleted = (code, language) => {
-  fetch(
-    `/add-snippet?code=${code}&language=${language}`,
-    { method: 'POST' }
-  );
+    fetch(
+        `/add-snippet?code=${code}&language=${language}`,
+        { method: 'POST' }
+    );
 }
 
 //fetches all snippets and creates json file from them 
-async function fetchSnippets() {
-  try {
-    const response = await fetch('/fetch-snippets');
-    if (!response.ok) {
-      throw new Error('Failed to fetch snippets');
+/*async function fetchSnippets() {
+    try {
+        const response = await fetch('/fetch-snippets');
+        if (!response.ok) {
+            throw new Error('Failed to fetch snippets');
+        }
+        return await response.json();
+    } catch (error) {
+        console.error('Error fetching snippets:', error);
+        return [];
     }
-    return await response.json();
-  } catch (error) {
-    console.error('Error fetching snippets:', error);
-    return [];
-  }
-}
-
+}*/
