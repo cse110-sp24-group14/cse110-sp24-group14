440d510bf64793e2314710f90a15f24f450ba847
dylan tran <dylantran3037@gmail.com>
2024-06-04 11:29:46 -0700

    Dylan / Journal Skeleton (#65)
    * JSDocs setup
    
    * Serve static html (#53)
    
    * Add files via upload
    
    * Create 052024-proj-sprint1-review.md
    
    * Create 052024-proj-sprint1-retro
    
    * Rename 052024-proj-sprint1-retro to 052024-proj-sprint1-retro.md
    
    * Delete admin/meetings/052024-proj-sprint-1.md
    
    * Update 052024-proj-sprint1-retro.md
    
    * Update jsdocs.yml
    
    * Create 053024-puppeteer.md ADR
    
    * Create 053024-sqlite3.md
    
    * Create 052924-ta-standup.md
    
    * created snippet skeleton
    
    * added TODO statement
    
    ---------
    
    Co-authored-by: tshneour <tshneour@ucsd.edu>
    Co-authored-by: Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
    Co-authored-by: Advaith Modali <80607809+advaithm22@users.noreply.github.com>
    Co-authored-by: Edward Lin <63514282+EdwardLinS@users.noreply.github.com>

--- source/server.js
+++ source/server.js
@@ -163,6 +163,7 @@ export const server = http.createServer((req, res) => {
         serveStaticFile(res, req.url.slice(1), 'text/javascript');
     } else if (req.url.endsWith('.html') && req.method === 'GET') {
         serveStaticFile(res, req.url.slice(1), 'text/html');
+
     // Add conditions for serving image files
     } else if (req.url.match(/\.(jpg|jpeg|png|gif|svg)$/) && req.method === 'GET') {
         const ext = path.extname(req.url).slice(1);
