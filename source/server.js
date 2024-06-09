8a641a47b0c254d67973d069e9a84fc902926d41
Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
2024-06-09 14:45:08 -0700

    Stable release 2024.06.09 14:40 (#143)
    * Advaith + Dylan / Navbar Data Update (#101)
    
    * initial commit for tasks portion of navbar update
    
    * removed snippets.css; already merged to snippetCreation.css
    
    * removed test
    
    * added update to header streaks
    
    * added displayStreaks scripts and stylesheet
    
    * removed a couple extra spaces
    
    * fixed pseudoUpdate for tasks completed
    
    * added inline and day 1 functionality
    
    * added single task functionality
    
    * added single task functionality
    
    * not needed
    
    * fixed bug with incorrect fire display
    
    * implement navbar update for number of incomplete tasks
    
    * remove buttons for support and settings, added "About Us" that redirects appropriately
    
    * fixes one day streak situation
    
    * add functionality to change incomplete count only if an already incomplete task is deleted
    
    ---------
    
    Co-authored-by: Advaith Modali <advaith0331@gmail.com>
    Co-authored-by: Advaith Modali <80607809+advaithm22@users.noreply.github.com>
    
    * Responsive main page (#128)
    
    * Bug fix one day forward
    
    * git pull recent staging, shows up with staged
    changes without commit
    
    * responsive main page
    
    * Minor styling changes with cell width and centering table
    
    ---------
    
    Co-authored-by: EdwardLinS <63514282+EdwardLinS@users.noreply.github.com>
    
    * Add link to dash btn (#140)
    
    * Fixed About us spacing and made responsive (#139)
    
    * added incomplete tasks update for navbar (#137)
    
    * resize dot so that it matches figma design (#141)
    
    ---------
    
    Co-authored-by: dylan tran <dylantran3037@gmail.com>
    Co-authored-by: Advaith Modali <advaith0331@gmail.com>
    Co-authored-by: Advaith Modali <80607809+advaithm22@users.noreply.github.com>
    Co-authored-by: OasisAlex <114462126+OasisAlex@users.noreply.github.com>
    Co-authored-by: EdwardLinS <63514282+EdwardLinS@users.noreply.github.com>
    Co-authored-by: tshneour <122496719+tshneour@users.noreply.github.com>

--- source/server.js
+++ source/server.js
@@ -79,7 +79,6 @@ const addStreaks = (callback) => {
     });
 };
 
-
 /**
  * Fetch all the tasks of for a specified date
  * 
@@ -121,7 +120,6 @@ const fetchNumSnippets = (callback) => {
     });
 };
 
-
 /**
  * Gets all dates of site visits
  * 
@@ -161,7 +159,7 @@ const fetchTasksDue = (year, month, callback) => {
         }
     });
 };
-          
+
 /**
  * Gets number of tasks that are completed from backend
  * 
@@ -186,6 +184,22 @@ const fetchNumberCompleted = (callback) => {
             callback(null, results);
         }
     })
+}
+
+/** 
+ * Gets number of incomplete tasks overall
+ * 
+ * @param {Function} callback
+ */
+const fetchNumIncompleteTasks = (callback) => {
+    const sqlQuery = 'SELECT COUNT(*) AS incomplete FROM Tasks WHERE completed = 0';
+    connection.query(sqlQuery, (error, results) => {
+        if (error) {
+            callback(error, null);
+        } else {
+            callback(null, results);
+        }
+    });
 }
 
 /**
@@ -268,7 +282,6 @@ const addSnippet = (code, language, date, callback) => {
         }
     });
 };
-
 
 /**
  * Fetches snippets from the SQL database by date
@@ -382,6 +395,16 @@ export const server = http.createServer((req, res) => {
                 res.end(JSON.stringify(numCompleted));
             }
         });
+    } else if (pathname === '/num-incomplete-tasks' && req.method === 'GET') {
+        fetchNumIncompleteTasks((err, users) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ error: 'Internal Server Error' }));
+            } else {
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(users));
+            }
+        });
     } else if (pathname === '/updated-task-completion' && req.method === 'PUT') {
         // updates the completed state of task
         const taskId = query.get('taskId');
@@ -460,7 +483,7 @@ export const server = http.createServer((req, res) => {
                 }
             });
         });
-    // Update the condition for serving CSS files
+        // Update the condition for serving CSS files
     } else if (pathname.endsWith('.css') && req.method === 'GET') {
         serveStaticFile(res, req.url.slice(1), 'text/css');
         // Update the condition for serving JavaScript files
