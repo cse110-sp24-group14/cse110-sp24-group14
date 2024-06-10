4b378096a93c4fc3a21c0847fb0755d3ce7b9708
a3alani <a3.alani@gmail.com>
2024-06-09 20:45:44 -0700

    Update welcome.js

--- source/welcomePage/welcome.js
+++ source/welcomePage/welcome.js
@@ -14,8 +14,42 @@ document.addEventListener("DOMContentLoaded", async function () {
 
         let completed_tasks = await response1.json();
         let todo_tasks = await response2.json();
-        let streak = await response3.json();
+        let streaks = await response3.json();
         let num_snippets = await response4.json();
+
+        /**
+             * streak calculation
+                ('2024-05-20')
+                ('2024-05-21')
+                ('2024-05-22')
+                ('2024-05-24')
+                ('2024-05-25')
+            
+            * start from the end of the visits list dates (most recent date)
+            * new Date() create a date object --> for example, May 25, 2024
+            * subtracting 2 date objects give the difference in milliseconds
+            * 1000 miliseconds = 1 second
+            * 60 seconds = 1 minute
+            * 60 minutes = 1 hour
+            */
+        streak_count = 1;
+        for (let i = streaks.length - 1; i > 1; i--) {
+        let curr_date = streaks[i];
+        let prev_date = streaks[i - 1];
+
+        console.log(curr_date);
+        console.log(prev_date);
+
+        let diff = curr_date - prev_date;
+
+        let diff_in_hours = diff / (1000 * 60 * 60);
+
+        if (diff_in_hours <= 24) {
+            streak_count++;
+        } else {
+            break;
+        }
+        }
 
         document.querySelector(".to-do .value").innerHTML =
         todo_tasks[0]["incomplete"];
@@ -23,8 +57,9 @@ document.addEventListener("DOMContentLoaded", async function () {
         document.querySelector(".completed .value").innerHTML =
         completed_tasks[0]["CompletedCount"];
 
-        document.querySelector(".streak .value").innerHTML = streak.length;
-        document.querySelector(".snippet .value").innerHTML = num_snippets.length;
+        document.querySelector(".streak .value").innerHTML = streak_count;
+        document.querySelector(".snippet .value").innerHTML =
+        num_snippets[0]["SnippetCount"];
     } catch (error) {
         console.log(error);
     }
